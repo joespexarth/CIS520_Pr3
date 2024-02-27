31be950fa620b63a6c61bc54a743a9d9de457d60
joespexarth <joespexarth@ksu.edu>
2024-02-27 00:27:58 -0600

    Block Store Create and Block Store Destroy working and passing test cases.

--- src/block_store.c
+++ src/block_store.c
@@ -9,47 +9,62 @@
 // remove it before you submit. Just allows things to compile initially.
 #define UNUSED(x) (void)(x)
 
+// I'm not sure if I defined this correctly but based off my understanding it makes sense, lmk if you have any questions (-Joseph Spexarth)
+typedef struct block_store {
+    uint8_t data[BLOCK_STORE_NUM_BLOCKS][BLOCK_SIZE_BYTES];
+    bitmap_t *bitmap; // Pointer to the bitmap
+}block_store_t;
+
 block_store_t *block_store_create()
 {
     // Malloc space for new BS
-    block_store_t * newBlock = malloc(BLOCK_STORE_NUM_BYTES);       
-    if(newBlock == NULL)
+    block_store_t * blockStore = (block_store_t*)malloc(BLOCK_STORE_NUM_BYTES);
+    // Ensure proper memory initalization       
+    if(blockStore == NULL)
     { 
         printf("Failed to malloc memory in BSC");
         return NULL; 
-    }
+    }     
     // Initialize the new BS to zero
-    memset(newBlock, 0, BLOCK_STORE_NUM_BYTES);                     
-    // Initialize the bitmap
-    //newBlock->bitmap = bitmap_overlay(BITMAP_SIZE_BITS, BITMAP_START_BLOCK);
-    /*
-    if(newBlock->bitmap == NULL)
+    memset(blockStore, 0, BLOCK_STORE_NUM_BYTES);                
+    // Initialize the bitmap -> project says FBM starts in block 127
+    blockStore->bitmap = bitmap_overlay(BITMAP_SIZE_BYTES * 8, (blockStore->data[126]) );
+    // Ensure proper memory initalization
+    if(blockStore->bitmap == NULL)
     {
         // Error handling if bitmap allocation fails
         printf("Failed to allocate bitmap");
-        free(newBlock); // Free allocated memory
+        block_store_destroy(blockStore); // Free allocated memory
         return NULL;
     }
-    // Mark the blocks used by the bitmap as allocated TODO: FIX this part is def incorrect
     
-    for (size_t i = 0; i < BITMAP_SIZE_BYTES; i++)
+    
+    // Mark the block used by the bitmap as allocated
+    block_store_request(blockStore, 126);
+
+    // Actual implementation for when block store request is implemented
+    /*
+    if (!block_store_request(blockStore, 126))
     {
-        if (!block_store_request(newBlock, BITMAP_START_BLOCK + i))
-        {
-            // Error handling if block allocation fails
-            printf("Failed to allocate block for bitmap");
-            bitmap_destroy(newBlock->bitmap); // Destroy bitmap overlay
-            free(newBlock); // Free allocated memory
-            return NULL;
-        }
+    // Error handling if block allocation fails
+    printf("Failed to allocate block for bitmap");
+    block_store_destroy(blockStore); // Free allocated memory
+    return NULL;
     }
     */
-    return newBlock;
+
+    return blockStore;
 }
 
 void block_store_destroy(block_store_t *const bs)
 {
-    UNUSED(bs);
+    // Assure we arnt waisting our time
+    if(bs == NULL) { return; }
+    // Free Bitmap
+    bitmap_destroy(bs->bitmap);
+    // Free Block Store
+    free(bs);
+    return;
 }
 size_t block_store_allocate(block_store_t *const bs)
 {
@@ -84,7 +99,8 @@ size_t block_store_get_free_blocks(const block_store_t *const bs)
 
 size_t block_store_get_total_blocks()
 {
-    return 0;
+    // Simply return the number of available user-addressable blocks
+    return BLOCK_STORE_AVAIL_BLOCKS;
 }
 
 size_t block_store_read(const block_store_t *const bs, const size_t block_id, void *buffer)
